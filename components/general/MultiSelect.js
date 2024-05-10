324792e94d5c876ef98a2c62fee69f1a7985c2dc
Deshan Dissanayake <113162275+deshanbdissanayake@users.noreply.github.com>
2024-05-10 16:23:13 +0530

    20240510-1623

---
+++ components/general/MultiSelect.js
@@ -0,0 +1,197 @@
+import { StyleSheet, Text, View, TouchableOpacity, TouchableWithoutFeedback, Modal, ScrollView } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { Entypo } from '@expo/vector-icons';
+import { colors } from '../../assets/colors/colors';
+
+const MultiSelect = ({ value, onSelect, placeholder, icon, options }) => {
+    const [showDropdown, setShowDropdown] = useState(false);
+    const [selectedOptions, setSelectedOptions] = useState([]);
+
+    useEffect(() => {
+        if (value !== '' && options) {
+            const selected = options.find((option) => option.value === value);
+            setSelectedOptions([selected]);
+        }
+    }, [value]);
+
+    const handleOptionSelect = (option) => {
+        const selectedIndex = selectedOptions.findIndex((selected) => selected.value === option.value);
+        if (selectedIndex === -1) {
+            // Option not selected, add to the list
+            setSelectedOptions([...selectedOptions, option]);
+        } else {
+            // Option already selected, remove from the list
+            const updatedOptions = [...selectedOptions];
+            updatedOptions.splice(selectedIndex, 1);
+            setSelectedOptions(updatedOptions);
+        }
+    };
+
+    const isOptionSelected = (option) => {
+        return selectedOptions.some((selected) => selected.value === option.value);
+    };
+
+    const closeModal = () => {
+        setShowDropdown(false);
+    };
+
+    const confirmSelection = () => {
+        onSelect(selectedOptions.map(option => option.value));
+        setShowDropdown(false);
+    };
+
+    const RenderDropdownItem = ({ item, index }) => {
+        const isSelected = isOptionSelected(item);
+
+        const itemStyles = [
+            styles.optionStyles,
+            isSelected && styles.selectedOption
+        ];
+
+        return (
+            <>
+                { !isSelected ? <View style={styles.optionBorderStyles}></View> : null }
+                <TouchableOpacity onPress={() => handleOptionSelect(item)} style={itemStyles}>
+                    <Text style={styles.optionTextStyles} numberOfLines={1}>
+                        {item.label}
+                    </Text>
+                </TouchableOpacity>
+            </>
+        );
+    };
+
+    return (
+        <View>
+            <TouchableWithoutFeedback onPress={() => setShowDropdown(true)}>
+                <View style={styles.selectWrapper}>
+                    <View style={styles.selectLeftWrapper}>
+                        {icon}
+                        <Text style={styles.selectText} numberOfLines={1}>
+                            {selectedOptions.length > 0 ? selectedOptions.map(option => option.label).join(', ') : placeholder}
+                        </Text>
+                    </View>
+                    <View style={styles.selectRightWrapper}>
+                        <Entypo name={showDropdown ? "chevron-up" : "chevron-down"} size={24} color={colors.textGraySecondary} />
+                    </View>
+                </View>
+            </TouchableWithoutFeedback>
+            <Modal 
+                visible={showDropdown} 
+                transparent={true} 
+                animationType="fade" 
+                onRequestClose={closeModal}
+            >
+                <TouchableWithoutFeedback onPress={closeModal}>
+                    <View style={styles.modalContainer}>
+                        <TouchableWithoutFeedback>
+                            <View style={styles.modalContent}>
+                                <ScrollView>
+                                    {(options !== null && options.length > 0) ?
+                                        options.map((option, index)=>(
+                                            <RenderDropdownItem item={option} index={index} key={index} closeModal={closeModal}/>
+                                        ))
+                                    : (
+                                        <RenderDropdownItem item={{value: 0, label: 'No Data'}} index={0} key={0} closeModal={closeModal}/>
+                                    )}
+                                </ScrollView>
+                                <TouchableOpacity onPress={confirmSelection}>
+                                    <Text style={styles.confirmButton}>Confirm</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </TouchableWithoutFeedback>
+                    </View>
+                </TouchableWithoutFeedback>
+            </Modal>
+        </View>
+    );
+};
+
+MultiSelect.propTypes = {
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string.isRequired,
+      value: PropTypes.any.isRequired,
+    })
+  ).isRequired,
+  placeholder: PropTypes.string
+};
+
+MultiSelect.defaultProps = {
+  placeholder: 'MultiSelect an option',
+};
+
+const styles = StyleSheet.create({
+    selectWrapper: {
+        borderWidth: 1,
+        borderColor: colors.border,
+        borderRadius: 10,
+        paddingVertical: 10,
+        paddingHorizontal: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    selectLeftWrapper: {
+        flex: 11,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    selectRightWrapper: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    selectText: {
+        fontSize: 14,
+        color: colors.textGraySecondary,
+        width: '100%',
+    },
+    dropdownContainer: {
+        borderColor: colors.border,
+        borderWidth: 1,
+        borderRadius: 10,
+        marginTop: 5,
+    },
+    optionStyles: {
+        paddingVertical: 15,
+        paddingHorizontal: 20,
+        width: '100%',
+        justifyContent: 'center',
+    },
+    optionBorderStyles: {
+        marginHorizontal: 10,
+        justifyContent: 'center',
+    },
+    optionTextStyles: {
+        fontSize: 14,
+        color: colors.textGraySecondary,
+    },
+    selectedOption: {
+        borderRadius: 5,
+        backgroundColor: colors.bgLight,
+    },
+    modalContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+    modalContent: {
+        width: 300,
+        maxHeight: 400,
+        backgroundColor: colors.white,
+        borderRadius: 5,
+    },
+    confirmButton: {
+        textAlign: 'center',
+        padding: 10,
+        backgroundColor: colors.primary,
+        color: colors.white,
+        borderRadius: 5,
+        marginTop: 10,
+    },
+});
+
+export default MultiSelect;
--- screens/ProductAddScreen.js
+++ screens/ProductAddScreen.js
@@ -4,6 +4,8 @@ import { colors } from '../assets/colors/colors'
 import Header from '../components/general/Header'
 import { useNavigation } from '@react-navigation/native'
 import Input from '../components/general/Input'
+import Button from '../components/general/Button'
+import MultiSelect from '../components/general/MultiSelect'
 
 const ProductAddScreen = ({ pro_id = null }) => {
   const navigation = useNavigation();
@@ -14,8 +16,17 @@ const ProductAddScreen = ({ pro_id = null }) => {
     navigation.goBack();
   }
 
+  const handleSubmitForm = () => {
+
+  }
+
   const [formData, setFormData] = useState({
     pro_name: null,
+    pro_desc: null,
+    pro_sku: null,
+    pro_price: null,
+    pro_sale_price: null,
+    pro_cat: [],
   });
   
   return (
@@ -25,31 +36,64 @@ const ProductAddScreen = ({ pro_id = null }) => {
         contentContainerStyle={styles.contentContainer}
         showsVerticalScrollIndicator={false}
       >
-        <View style={styles.formGroup}>
-          <Text style={styles.labelTextStyles}>Product Name</Text>
-          <Input
-            keyboardType={'default'}
-            value={formData.pro_name ?? ''}
-            onChangeText={(text) => setFormData((prevData) => ({ ...prevData, pro_name: text }))}
-            placeholder={'Enter Product Name'}
-          />
+        <View style={styles.formStyles}>
+          <View style={styles.formGroup}>
+            <Text style={styles.labelTextStyles}>Product Name</Text>
+            <Input
+              keyboardType={'default'}
+              value={formData.pro_name ?? ''}
+              onChangeText={(text) => setFormData((prevData) => ({ ...prevData, pro_name: text }))}
+              placeholder={'Enter Product Name'}
+            />
+          </View>
+          <View style={styles.formGroup}>
+            <Text style={styles.labelTextStyles}>Product Description</Text>
+            <Input
+              keyboardType={'default'}
+              value={formData.pro_desc ?? ''}
+              onChangeText={(text) => setFormData((prevData) => ({ ...prevData, pro_desc: text }))}
+              placeholder={'Enter Product Description'}
+              multiline={true}
+              textArea={true}
+            />
+          </View>
+          <View style={styles.formGroup}>
+            <Text style={styles.labelTextStyles}>Product SKU</Text>
+            <Input
+              keyboardType={'default'}
+              value={formData.pro_sku ?? ''}
+              onChangeText={(text) => setFormData((prevData) => ({ ...prevData, pro_sku: text }))}
+              placeholder={'Enter Product SKU'}
+            />
+          </View>
+          <View style={styles.formGroup}>
+            <Text style={styles.labelTextStyles}>Product Price</Text>
+            <Input
+              keyboardType={'numeric'}
+              value={formData.pro_price ?? ''}
+              onChangeText={(text) => setFormData((prevData) => ({ ...prevData, pro_price: text }))}
+              placeholder={'Enter Product Price'}
+            />
+          </View>
+          <View style={styles.formGroup}>
+            <Text style={styles.labelTextStyles}>Product Sale Price</Text>
+            <Input
+              keyboardType={'numeric'}
+              value={formData.pro_sale_price ?? ''}
+              onChangeText={(text) => setFormData((prevData) => ({ ...prevData, pro_sale_price: text }))}
+              placeholder={'Enter Product Sale Price'}
+            />
+          </View>
+          <View style={styles.formGroup}>
+            <Text style={styles.labelTextStyles}>Product Category</Text>
+            
+          </View>
         </View>
-        <View style={styles.formGroup}>
-          <Text style={styles.labelTextStyles}>Product Name</Text>
-          <Input
-            keyboardType={'default'}
-            value={formData.pro_name ?? ''}
-            onChangeText={(text) => setFormData((prevData) => ({ ...prevData, pro_name: text }))}
-            placeholder={'Enter Product Name'}
-          />
-        </View>
-        <View style={styles.formGroup}>
-          <Text style={styles.labelTextStyles}>Product Name</Text>
-          <Input
-            keyboardType={'default'}
-            value={formData.pro_name ?? ''}
-            onChangeText={(text) => setFormData((prevData) => ({ ...prevData, pro_name: text }))}
-            placeholder={'Enter Product Name'}
+        <View style={styles.btnStyles}>
+          <Button
+            bgColor={colors.bgColorSec}
+            content={<Text style={{ color: colors.textColorSec, fontFamily: 'ms-regular'}}>Submit</Text>}
+            func={handleSubmitForm}
           />
         </View>
       </ScrollView>
@@ -68,6 +112,7 @@ const styles = StyleSheet.create({
   },
   contentContainer: {
     flexGrow: 1,
+    justifyContent: 'space-between',
   },
   formGroup: {
     marginBottom: 15,
